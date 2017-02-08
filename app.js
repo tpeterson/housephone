d8b7225e1d571bff474a7a7dc676c6718a57b280
Tim <tpeterson@users.noreply.github.com>
2017-02-08 08:48:53 -0800

    Add code to for basic send and receive handling.

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser');
 const crypto = require('crypto');
 const request = require('request');
 
-const VALIDATION_TOKEN = process.env.VERIFY_TOKEN || process.env.MESSENGER_HOUSEPHONE_VERIFY;
-const PAGE_TOKEN = process.env.PAGE_TOKEN || process.env.MESSENGER_HOUSEPHONE_PAGE;
-const APP_SECRET = process.env.APP_SECRET || process.env.MESSENGER_HOUSEPHONE_SECRET;
+const VALIDATION_TOKEN = process.env.VERIFY_TOKEN;
+const PAGE_TOKEN = process.env.PAGE_TOKEN;
+const APP_SECRET = process.env.APP_SECRET;
 
 const app = express();
 app.use(bodyParser.json({
@@ -51,5 +51,98 @@ app.get('/webhook', function getWebhook(req, res) {
     res.sendStatus(403);
   }
 });
+// Handle messages to and from Messenger bot
+app.post('/webhook', function postWebhook(req, res) {
+  let data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      let pageID = entry.id;
+      let timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log('Webhook received unknown event: ', event);
+        }
+      });
+    });
+
+    res.sendStatus(200);
+  }
+});
+// ACT ON MESSAGE RECEIVED FROM USER
+function receivedMessage(event) {
+  // Retrieve Facebook user ID of sender
+  const sender = event.sender.id;
+
+  // Retrieve session info if existing user or create new session
+  //const sessionId = findOrCreateSession(sender);
+
+  const timeOfMessage = event.timestamp;
+
+  // Retrieve message content
+  const {
+    text,
+    attachments
+  } = event.message;
+
+  console.log(`Received message from user ${sender} at ${timeOfMessage} with message: ${JSON.stringify(text)}`);
+
+  if (text) {
+    // Check content of text message
+    switch (text) {
+      default:
+        // WILL REPLACE WITH WIT.RUNACTIONS
+        sendTextMessage(sender, 'Hi, I\'m still getting set up.');
+    }
+  } else if (attachments) {
+    sendTextMessage(sender, 'I can\'t handle attachments yet.');
+  }
+}
+// SEND BASIC TEXT MESSAGE
+function sendTextMessage(recipientId, messageText) {
+  // Add recipient and message text to message object
+  let messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  // Pass message object to send function
+  callSendAPI(messageData);
+}
+//SEND MESSAGE BACK TO USER
+function callSendAPI(messageData) {
+  // Make post request to Facebook Send API
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: PAGE_TOKEN
+    },
+    method: 'POST',
+    json: messageData
+
+  }, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      let recipientId = body.recipient_id;
+      let messageId = body.message_id;
+
+      console.log(`Successfully sent message with ID ${messageId} to recipient ${recipientId}`);
+    } else {
+      console.error('Unable to send message.');
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
 
 app.listen(process.env.PORT || 5000);
