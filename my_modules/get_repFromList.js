a1017f228965fac3d06608255e45724147a10fa2
Tim <tpeterson@users.noreply.github.com>
2017-02-13 11:23:15 -0800

    Fix bug when last name is more than 1 word, like Lastname Jr.

--- my_modules/get_repFromList.js
+++ my_modules/get_repFromList.js
@@ -1,10 +1,7 @@
 function lookUpRep(rep_name, listOfReps) {
-  const rep_nameArr = rep_name.trim().split(' ');
-  const rep_firstName = rep_nameArr[0];
-  const rep_lastName = rep_nameArr[rep_nameArr.length - 1];
-
   const foundRep = listOfReps.filter((repObj) => {
-    return repObj.firstName.includes(rep_firstName) && (repObj.lastName === rep_lastName);
+    const repObj_fullName = repObj.firstName + ' ' + repObj.lastName;
+    return repObj_fullName === rep_name.trim();
   });
   return foundRep[0];
 }
