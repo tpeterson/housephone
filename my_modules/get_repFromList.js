53b79ba84bbdd7a13c14e5412139076aa404df2b
Tim <tpeterson@users.noreply.github.com>
2017-02-13 12:01:59 -0800

    Fix bug when names from House directory may not fully match names from House zip code search.

--- my_modules/get_repFromList.js
+++ my_modules/get_repFromList.js
@@ -1,7 +1,11 @@
 function lookUpRep(rep_name, listOfReps) {
+  const rep_nameArr = rep_name.trim().split(' ');
+  const rep_firstName = rep_nameArr[0];
+  const rep_lastName = rep_nameArr[rep_nameArr.length - 1];
+
   const foundRep = listOfReps.filter((repObj) => {
     const repObj_fullName = repObj.firstName + ' ' + repObj.lastName;
-    return repObj_fullName === rep_name.trim();
+    return (repObj.firstName.includes(rep_firstName) && (repObj.lastName === rep_lastName)) || (repObj_fullName === rep_name.trim());
   });
   return foundRep[0];
 }
